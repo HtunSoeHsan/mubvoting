75f2efc2bd555e284b88e25220a3eca97f634485
Tun72 <tunt72553@gmail.com>
2024-12-07 16:10:34 +0630

    send-otp-code-api

---
+++ src/db.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
+
+const prisma = new PrismaClient();
+
+export const db = prisma;
